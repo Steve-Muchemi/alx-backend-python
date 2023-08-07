9a8a0ebd8ec839a9ac4b860aad1481c59b6a273b
stewie-gil <gilliganstewie7@gmail.com>
2023-08-07 07:48:21 -0700

    added a comment

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -7,5 +7,7 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 def task_wait_random(max_delay: int) -> Task:
+    """ Arg: max_delay - max time the wait random will delay
+    return: task - an async task"""
     task: Task = create_task(wait_random(max_delay))
     return task
