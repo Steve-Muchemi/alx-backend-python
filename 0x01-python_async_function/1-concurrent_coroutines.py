95b26d9e9e1149e8d86821d0ec9c553f67eb37a9
stewie-gil <gilliganstewie7@gmail.com>
2023-08-07 04:24:32 -0700

    for loop with async

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+""" This module contains the function wait_n """
+
+
+import asyncio
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Args: 
+      max_delay: maximum time to be generated
+    Returns: 
+      a list of float values for time btween 0 and max-delay
+    """
+    array: List[float] = []
+    for i in range(n):
+        a: float = await wait_random(max_delay)
+        array.append(a)
+    return array
