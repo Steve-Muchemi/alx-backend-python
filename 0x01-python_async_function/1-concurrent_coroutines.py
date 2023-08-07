c146117a64c25ab0e0a054ca1accdebcc5da40e3
stewie-gil <gilliganstewie7@gmail.com>
2023-08-07 06:51:38 -0700

    async as completed

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -9,13 +9,13 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """
-    Args: 
-      max_delay: maximum time to be generated
-    Returns: 
+    Args:
+    max_delay: maximum time to be generated
+    Returns:
       a list of float values for time btween 0 and max-delay
     """
     array: List[float] = []
     for i in range(n):
-        a: float = await wait_random(max_delay)
-        array.append(a)
-    return array
+        task = wait_random(max_delay)
+        array.append(task)
+    return [await i for i in asyncio.as_completed(array)]
