27e3c02624a0ea4e185ab9cfa01288da9e0b9ab4
stewie-gil <gilliganstewie7@gmail.com>
2023-08-07 08:25:20 -0700

    tasks

---
+++ 0x01-python_async_function/4-tasks.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+""" This module contains the function wait_n """
+
+
+import asyncio
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Args:
+    max_delay: maximum time to be generated
+    Returns:
+      a list of float values for time btween 0 and max-delay
+    """
+    array: List[float] = []
+    for i in range(n):
+        task = wait_random(max_delay)
+        array.append(task)
+    return [await i for i in asyncio.as_completed(array)]
