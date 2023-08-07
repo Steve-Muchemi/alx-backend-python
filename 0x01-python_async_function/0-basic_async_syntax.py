1f20dae064905c978ca2163589d0efaee8ebb9bb
stewie-gil <gilliganstewie7@gmail.com>
2023-08-07 03:33:39 -0700

    annotation fix

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -8,6 +8,6 @@ import random
 
 async def wait_random(mandatory: int = 10) -> float:
     """a simple wait function returns seconds waited"""
-    wait = random.uniform(0, mandatory)
+    wait: float = random.uniform(0, mandatory)
     await asyncio.sleep(wait)
     return wait
